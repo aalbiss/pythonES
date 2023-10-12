10a9f3fedd8431da34b0ad1c8fbee2f5ebc58b83
aalbiss <albertoambrosi6@gmail.com>
2023-10-12 09:35:26 +0200

    giorno 12/10/2023

---
+++ es24pag73/es24pag73.py
@@ -0,0 +1,5 @@
+"""
+si acquisciono i vori ottenuti da 2 candidati.
+Scrivere il programma che calcoli le percentuali ottenute da ciascun candidato
+"""
+
---
+++ es24pag73/funzioni.py
@@ -0,0 +1,13 @@
+def inserimentoVoti():
+    nomiVoti=[[],[]]
+    for i in range(2):
+        nomiVoti[0].append(input("Inserisci nome: "))
+        voto=int(input("Inserisci voto: "))
+        while(voto>=19):
+            voto=int(input("Inserisci voto: "))
+        nomiVoti[1].append(voto)
+    return nomiVoti
+
+
+def percentualeOttenuta():
+    
